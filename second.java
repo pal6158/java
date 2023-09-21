179e15c186f13babcb811315a366373c45054075
pal6158 <pal61586@gmail.com>
2023-09-21 19:07:36 +0530

    oddEvenByfunction

--- second.java
+++ second.java
@@ -1,5 +1,22 @@
-public class second {
-    public static void main(String args[]) {
-        System.out.println("aka");
+import java.util.Scanner;
+
+class Solution{
+    static String oddEven(int N){
+        if (N % 2 == 0) {
+            
+            return "even";
+        
+        }else{
+            return "odd";
+        }
+        
+        
+        // code here
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int p = sc.nextInt();
+        System.out.println( oddEven(p) ); 
+        
     }
 }
